d4266e2617ccf7f98d1193247bcfe69ba8aaa3dd
Duck <CDUCK@corp.vha.ad>
2017-04-14 16:20:35 -0500

    Sample handler was invoked before functions were defined.

--- src/private/_classes.ps1
+++ src/private/_classes.ps1
@@ -8,4 +8,4 @@ Add-Type -Path $NancyPSRoot\lib\Nancy.Hosting.Self.dll
 Add-Type -Path $NancyPSRoot\NancyPS.cs -ReferencedAssemblies Microsoft.CSharp, ([Nancy.NancyModule].Assembly)
 
 # You should add more of these ...
-Set-NancyHandler -Path "/" -Handler { "Welcome to Nancy" }
+#Set-NancyHandler -Path "/" -Handler { "Welcome to Nancy" }
