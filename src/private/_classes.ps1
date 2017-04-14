d5895059e3a62d158c5a4c7bd087c8b29713e651
Duck <CDUCK@corp.vha.ad>
2017-04-14 16:19:55 -0500

    Using was not copied from psm1

--- src/private/_classes.ps1
+++ src/private/_classes.ps1
@@ -2,6 +2,9 @@ if(!$NancyPSRoot) {
     $NancyPSRoot = $PSScriptRoot
 }
 
+Add-Type -Path $NancyPSRoot\lib\Nancy.dll
+Add-Type -Path $NancyPSRoot\lib\Nancy.Hosting.Self.dll
+
 Add-Type -Path $NancyPSRoot\NancyPS.cs -ReferencedAssemblies Microsoft.CSharp, ([Nancy.NancyModule].Assembly)
 
 # You should add more of these ...
